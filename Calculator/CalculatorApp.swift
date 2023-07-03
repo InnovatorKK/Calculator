ff95737c2cb60bd5cc3a808f477aaabcea9ef73d
Juhyun-KK <juhyunk1119@gmail.com>
2023-07-03 22:32:10 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Juhyun Kang on 2023/07/03.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
